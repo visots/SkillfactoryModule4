78711a622d891f37afb8eaadeb5510b3839b8e12
Alexey <visotskiy.alexey@gmail.com>
2023-01-18 22:58:04 +0700

    Добавлена обработка цвета cyan, изменено содержимое кейса default

--- 4-1-17/Program.cs
+++ 4-1-17/Program.cs
@@ -8,29 +8,36 @@
 
 			var color = Console.ReadLine();
 
-			if (color == "red")
+			switch (color)
 			{
-				Console.BackgroundColor = ConsoleColor.Red;
-				Console.ForegroundColor = ConsoleColor.Black;
+				case "red":
+					Console.BackgroundColor = ConsoleColor.Red;
+					Console.ForegroundColor = ConsoleColor.Black;
 
-				Console.WriteLine("Your color is red!");
-			}
+					Console.WriteLine("Your color is red!");
+					break;
 
-			else if (color == "green")
-			{
-				Console.BackgroundColor = ConsoleColor.Green;
-				Console.ForegroundColor = ConsoleColor.Black;
+				case "green":
+					Console.BackgroundColor = ConsoleColor.Green;
+					Console.ForegroundColor = ConsoleColor.Black;
 
-				Console.WriteLine("Your color is green!");
-			}
-			else
-			{
-				Console.BackgroundColor = ConsoleColor.Cyan;
-				Console.ForegroundColor = ConsoleColor.Black;
+					Console.WriteLine("Your color is green!");
+					break;
+				case "cyan":
+					Console.BackgroundColor = ConsoleColor.Cyan;
+					Console.ForegroundColor = ConsoleColor.Black;
 
-				Console.WriteLine("Your color is cyan!");
+					Console.WriteLine("Your color is cyan!");
+					break;
+
+
+				default:
+					Console.BackgroundColor = ConsoleColor.Yellow;
+					Console.ForegroundColor = ConsoleColor.Red;
+
+					Console.WriteLine("Your color is cyan!");
+					break;
 			}
 		}
-	}
     }
 }
