40959b1f00565368fbcbfdc5cadcc953a1d6fd24
Alexey <visotskiy.alexey@gmail.com>
2023-01-24 23:51:15 +0700

    Задание 5-1-6 одним заданием сделал несколько других в т.ч. 5-2

---
+++ 5/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = GetArrayFromConsole();
+            ShowArray(result);
+            result = SortArray(result);
+            ShowArray(result);
+        }
+
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            Console.WriteLine("Введите пять цифр через запятую:");
+
+            var userValues = Console.ReadLine().Split(',');
+
+            for (int i = 0; i < userValues.Length; i++)
+                result[i] = Int32.Parse(userValues[i]);
+
+
+
+            return result;
+        }
+
+        static int[] SortArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                for (int k = i + 1; k < arr.Length; k++)
+                {
+                    if (arr[i] > arr[k])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[k];
+                        arr[k] = temp;
+                    }
+                }
+
+            return arr;
+        }
+
+        static void ShowArray( int[] arr)
+        {
+            foreach(var el in arr)
+                Console.Write(el);
+            Console.WriteLine(); 
+        }
+    }
+}
