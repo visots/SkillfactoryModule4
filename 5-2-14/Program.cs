566eab9d491c97fadd538b993de47437ec61e4d4
Alexey <visotskiy.alexey@gmail.com>
2023-01-26 23:53:08 +0700

    Задания модуля 5

---
+++ 5-2-14/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace _5_2_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = GetArrayFromConsole();
+            ShowArray(result, true);
+        }
+
+        static int[] GetArrayFromConsole(int arrLength = 5)
+        {
+            var result = new int[arrLength];
+
+            Console.WriteLine($"Введите {arrLength} цифр через запятую:");
+
+            var userValues = Console.ReadLine().Split(',');
+
+            for (int i = 0; i < userValues.Length; i++)
+                result[i] = Int32.Parse(userValues[i]);
+
+
+
+            return result;
+        }
+
+        static int[] SortArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                for (int k = i + 1; k < arr.Length; k++)
+                {
+                    if (arr[i] > arr[k])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[k];
+                        arr[k] = temp;
+                    }
+                }
+
+            return arr;
+        }
+
+        static void ShowArray(int[] arr, bool isSort = false)
+        {
+            if (isSort)
+                arr = SortArray(arr);
+
+            foreach (var el in arr)
+                Console.Write(el);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 5.3.13/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace _5._3._13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int[] array = new int[] { 3, 5, 1, 7, 9 };
+
+            int[] sortedAsc = new int[] { };
+            int[] sortedDesc = new int[] { };
+            SortArray( array,out sortedAsc, out sortedDesc);
+            
+            ShowArray(sortedAsc);
+            ShowArray(sortedDesc);
+
+        }
+
+        static void SortArray(in int[] array,out int [] sortedAsc,out int [] sortedDesc)
+        {
+            sortedDesc = SortArrayDesc(array);
+            sortedAsc = SortArrayAsc(array);
+        }
+
+        static int[] SortArrayAsc(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                for (int k = i + 1; k < arr.Length; k++)
+                {
+                    if (arr[i] > arr[k])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[k];
+                        arr[k] = temp;
+                    }
+                }
+            return arr;
+        }
+
+        static int[] SortArrayDesc(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+                for (int k = i + 1; k < arr.Length; k++)
+                {
+                    if (arr[i] < arr[k])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[k];
+                        arr[k] = temp;
+                    }
+                }
+            return arr;
+        }
+
+        static void ShowArray(int[] arr)
+        {
+            foreach (var el in arr)
+                Console.Write(el);
+            Console.WriteLine();
+        }
+    }
+}
